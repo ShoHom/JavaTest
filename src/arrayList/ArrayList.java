b50bac7d6c6bc07f4d837ef2d6e4b069159cd23c
ShoHom <394282530@qq.com>
2020-10-30 18:11:51 +0800

    A new commitss

--- src/arrayList/ArrayList.java
+++ src/arrayList/ArrayList.java
@@ -1,126 +1,100 @@
 package arrayList;
+//顺序表代码
 
-import java.util.Arrays;
+public class ArrayList<T> {
+    private T[] eles;
+    //记录当前顺序表中的元素个数
+    private int N;
 
-public class ArrayList<E> {
-    //定义数组的默认容量为10
-    private static final int DEFAULT_CAPACITY = 10;
-    //定义默认空数组
-    private static final Object[] EMPTY_ELEMENTDATA = {};
-    //定义空数组，用于数组判空或者置空
-    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
-    //定义缓存数组，并不可序列化
-    transient Object[] elementData;
-    //数组长度
-    private int size;
-    //定义最大数组长度
-    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
-
-    //获取数组长度的方法
-    public int size() {
-        return size;
-    }
-
-    //----------------------扩容-----------------------
-
-    //构造器构造默认数组长度为空的数组
+    //构造方法,构建数组时默认长度为1
     public ArrayList() {
-        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+        eles = (T[]) new Object[1];
+        N = 0;
     }
 
-    //数组添加元素方法
-    public boolean add(E e) {
-        ensureCapacityInternal(size + 1);  // Increments modCount!!
-        elementData[size++] = e;
-        return true;
+    //将一个线性表置为空表
+    public void clear() {
+        N = 0;
     }
 
-    //计算数组装纳元素所需的最小容量
-    private static int calculateCapacity(Object[] elementData, int minCapacity) {
-        if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
-            return Math.max(DEFAULT_CAPACITY, minCapacity);
+    //判断当前线性表是否为空表
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    //获取线性表的长度
+    public int length() {
+        return N;
+    }
+
+    //获取指定位置的元素
+    public T get(int i) {
+        if (i < 0 || i >= N) {
+            throw new RuntimeException("当前元素不存在！");
         }
-        return minCapacity;
+        return eles[i];
     }
 
-    //确认内部容量
-    private void ensureCapacityInternal(int minCapacity) {
-        ensureExplicitCapacity(calculateCapacity(elementData, minCapacity));
+    //向线型表中添加元素t
+    public void add(T t) {
+        if (N == eles.length) {
+            resize(eles.length * 2);
+        }
+        eles[N++] = t;
     }
 
-    //确认容量是否合法
-    private void ensureExplicitCapacity(int minCapacity) {
-        if (minCapacity - elementData.length > 0)
-            grow(minCapacity);
+    //在i元素处插入元素t
+    public void add(int i,T t) {
+        if (i < 0 || i > N) {
+            throw new RuntimeException("插入的位置不合法");
+        }
+        //元素已经放满了数组，需要扩容
+        if (N == eles.length) {
+            resize(eles.length * 2);
+        }
+        for (int index=N-1;index>i;index--){
+            eles[index]=eles[index-1];
+        }
+        //把t放到i位置处
+        eles[i]=t;
+        //元素数量+1
+        N++;
     }
 
-    //扩容方法
-    private void grow(int minCapacity) {
-        //将缓存数组容量赋值为旧容量
-        int oldCapacity = elementData.length;
-        //将新数组容量赋值为旧容量的1.5倍 通过位运算符来计算的原因是：提高运算效率
-        int newCapacity = oldCapacity + (oldCapacity >> 1);
-        if (newCapacity - minCapacity < 0)
-            newCapacity = minCapacity;
-        //这里对容量是否超过数组最大容量值进行了判断
-        if (newCapacity - MAX_ARRAY_SIZE > 0)
-            newCapacity = hugeCapacity(minCapacity);
-        //对数组元素进行复制，容量为newCapacity
-        elementData = Arrays.copyOf(elementData, newCapacity);
+        //删除指定位置i处的元素，并返回该元素
+    public T remove(int i) {
+        if (i < 0 || i > N - 1) {
+            throw new RuntimeException("当前要删除的元素不存在");
+        }
+        //记录i位置处的元素
+        T result = eles[i];
+        //把i位置后面的元素都向前移动一位
+        for (int index = i; index < N - 1; index++) {
+            eles[index] = eles[index + 1];
+        }
+        //当前元素数量-1
+        N--;
+        //当元素已经不足数组大小的1/4,则重置数组的大小
+        if (N > 0 && N < eles.length / 4) {
+            resize(eles.length / 2);
+        }
+        return result;
     }
 
-    private static int hugeCapacity(int minCapacity) {
-        if (minCapacity < 0) // overflow
-            throw new OutOfMemoryError();
-        return (minCapacity > MAX_ARRAY_SIZE) ?
-                Integer.MAX_VALUE :
-                MAX_ARRAY_SIZE;
-    }
-
-    //--------------------缩容-----------------------
-
-    //移除元素方法
-    public E remove(int index) {
-        rangeCheck(index);
-        E oldValue = elementData(index);
-        int numMoved = size - index - 1;
-        if (numMoved > 0)
-            System.arraycopy(elementData, index + 1, elementData, index,
-                    numMoved);
-        elementData[--size] = null;
-        trimToSize();
-        return oldValue;
-    }
-
-    //越界判断
-    private void rangeCheck(int index) {
-        if (index >= size)
-            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
-    }
-
-    private String outOfBoundsMsg(int index) {
-        return "Index: " + index + ", Size: " + size;
-    }
-
-    E elementData(int index) {
-        return (E) elementData[index];
-    }
-
-    //缩容方法
-    public void trimToSize() {
-        // 判断当前是否需要缩容
-        if (size < elementData.length) {
-            // 如果size为0，直接给elementData赋值内置的空数组
-            // 不为0则创建一个size长度的新数组
-            elementData = (size == 0)
-                    ? EMPTY_ELEMENTDATA
-                    : Arrays.copyOf(elementData, size);
+    //改变容量
+    private void resize(int newSize) {
+        //记录旧数组
+        T[] temp = eles;
+        //创建新数组
+        eles = (T[]) new Object[newSize];
+        //把旧数组中的元素拷贝到新数组
+        for (int i = 0; i < N; i++) {
+            eles[i] = temp[i];
         }
     }
 
-    //---------------------获取指定下标元素-------------------
-    public E get(int index) {
-        rangeCheck(index);
-        return elementData(index);
+    public int capacity() {
+        return eles.length;
     }
+
 }
